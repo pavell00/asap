48bf460d491b34f72a37c841978a4f9f3811be7b
Bratushka Pavel <Bratushka.P@blg.local>
2018-10-10 16:26:08 +0300

    3minutes live  time token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,11 +4,11 @@ const config =  require('../config');
 const checkAuth = (req, res, next) => {
 	var token = req.headers['token'];
 	if (!token)
-		return res.status(403).send({ auth: false, message: 'No token provided.' });
+		return res.status(403).send({ success: false, message: 'No token provided.' });
 
 	jwt.verify(token, config.jwtSecret, (err, decoded) => {
 		if (err)
-			return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+			return res.status(500).send({ success: false, message: 'Failed to authenticate token.' });
 
     req.user = {
 			login: decoded.login,
